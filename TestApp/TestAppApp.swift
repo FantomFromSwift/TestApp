dfa47f671273a9f0370814c094dddc0501ce6f85
Fantom <ivanneporadnyj@gmail.com>
2025-05-07 14:10:00 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Иван Непорадный on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
